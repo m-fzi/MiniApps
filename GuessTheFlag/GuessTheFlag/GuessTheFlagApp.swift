4d8593c2a1cf2874bbcd71aa3ed83c35d26e4bd7
m-fzi <mfzkgl@gmail.com>
2021-11-01 12:01:25 +0300

    It seems like you may want to improve your flags knowledge

---
+++ GuessTheFlag/GuessTheFlag/GuessTheFlagApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuessTheFlagApp.swift
+//  GuessTheFlag
+//
+//  Created by f on 27.10.2021.
+//
+
+import SwiftUI
+
+@main
+struct GuessTheFlagApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
