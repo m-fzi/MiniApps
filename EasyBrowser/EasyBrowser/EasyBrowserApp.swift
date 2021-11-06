fa983b7de2781b370d20479d47419deb79e70ec1
m-fzi <mfzkgl@gmail.com>
2021-11-06 15:22:54 +0300

    n

---
+++ EasyBrowser/EasyBrowser/EasyBrowserApp.swift
@@ -0,0 +1,17 @@
+//
+//  EasyBrowserApp.swift
+//  EasyBrowser
+//
+//  Created by f on 4.11.2021.
+//
+
+import SwiftUI
+
+@main
+struct EasyBrowserApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ WordScramble/WordScramble/WordScrambleApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordScrambleApp.swift
+//  WordScramble
+//
+//  Created by f on 5.11.2021.
+//
+
+import SwiftUI
+
+@main
+struct WordScrambleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            WordScrambleView()
+        }
+    }
+}
