cd1ae68dcb9722e004ce375848025cf6cf1d192f
m-fzi <mfzkgl@gmail.com>
2021-11-28 14:06:29 +0300

    n

---
+++ iExpense/iExpense/iExpenseApp.swift
@@ -0,0 +1,17 @@
+//
+//  iExpenseApp.swift
+//  iExpense
+//
+//  Created by f on 23.11.2021.
+//
+
+import SwiftUI
+
+@main
+struct iExpenseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
